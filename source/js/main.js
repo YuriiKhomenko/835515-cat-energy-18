1a53761f9e987e83eb367facfa7bcba2199fb6ad
Yurii Khomenko <yuriikhomenko@macBookProYurii.home>
2020-01-24 20:27:21 +0100

    Добавляет изменения перед защитой

--- source/js/main.js
+++ source/js/main.js
@@ -4,13 +4,13 @@ var navigation = document.querySelector(".main-nav__list");
 
 if (button) {
   button.addEventListener("click", function() {
-    if (button.classList.contains("page-header--open")) {
-      button.classList.remove("page-header--open");
-      button.classList.add("page-header--close");
+    if (button.classList.contains("page-header__menu-button--open")) {
+      button.classList.remove("page-header__menu-button--open");
+      button.classList.add("page-header__menu-button--close");
       navigation.style.display = "block";
     } else {
-      button.classList.remove("page-header--close");
-      button.classList.add("page-header--open");
+      button.classList.remove("page-header__menu-button--close");
+      button.classList.add("page-header__menu-button--open");
       navigation.style.display = "none";
     }
   });
@@ -21,17 +21,17 @@ var imgBefore = document.querySelector(".example__img--before");
 var imgAfter = document.querySelector(".example__img--after");
 var before = document.querySelector(".example__before");
 var after = document.querySelector(".example__after");
-var sliderSwitchThumb = document.querySelector(".slider__switch-thumb");
+var sliderSwitchThumb = document.querySelector(".slider__switch");
 
 if (before && after) {
   before.addEventListener("click", function() {
-    sliderSwitchThumb.style.float = "left";
+    sliderSwitchThumb.style.justifyContent = "flex-start";
     imgAfter.style.display = "none";
     imgBefore.style.display = "block";
   });
 
   after.addEventListener("click", function() {
-    sliderSwitchThumb.style.float = "right";
+    sliderSwitchThumb.style.justifyContent = "flex-end";
     imgBefore.style.display = "none";
     imgAfter.style.display = "block";
   });
